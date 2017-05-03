43a4b8e19d750e4d77c84c94027458c8545fba6c
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-03 16:28:07 -0700

    Update arguments passed to greenify

--- public/snippets/parameters-and-arguments.js
+++ public/snippets/parameters-and-arguments.js
@@ -2,4 +2,6 @@ const greenify = function(el) {
   el.setAttribute('style', 'background-color: green');
 };
 
-greenify();
+const el = document.querySelectorAll('TH');
+greenify(el[2]);
+greenify(el[3]);

ba78a298fb2bc295bc55cf7e536eccb08dd51873
mttrys <mttrys@gmail.com>
2017-05-03 16:20:04 -0700

    Work in Progress. Objects Methods Clock. Gets time wont render

--- public/snippets/clock.js
+++ public/snippets/clock.js
@@ -1,7 +1,14 @@
+let date = new Date();
+
 const clock = {
-  getHours: null,
-  getMinutes: null,
-  getSeconds: null
+  getHours: date.getHours(),
+  getMinutes: date.getMinutes(),
+  getSeconds: date.getSeconds()
 };
 
+// the function gets the current time but
+// it does not render in methods.html
 
+console.log(clock.getHours)
+console.log(clock.getMinutes)
+console.log(clock.getSeconds)

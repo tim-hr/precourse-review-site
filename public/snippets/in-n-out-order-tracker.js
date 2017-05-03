45b698efece57380e372e58861ac9103562b68b9
Michael Clausen <michaeljclausen@gmail.com>
2017-05-03 16:30:55 -0700

    complete classes:state and behavior

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,12 +1,35 @@
 class Order {
 
+  constructor() {
+    this.orderStatus = {0: 'Taking order',
+                  1: 'Cooking order',
+                  2: 'Requesting pickup of order',
+                  3: 'Order complete'};
+    this.state = 0;
+  }
+  
+  advance() {
+    this.state++;
+  }
+
+  status() {
+    console.log(this.orderStatus[this.state]);
+  }
 }
 
 // create order instance #1
+let order1 = new Order();
 // create order instance #2
+let order2 = new Order();
 // advance state of #1
+order1.advance();
 // advance state of #1
+order1.advance();
 // advance state of #2
+order2.advance();
 // advance state of #1
+order1.advance();
 // print state of #1 to console
+order1.status();
 // print state of #2 to console
+order2.status();

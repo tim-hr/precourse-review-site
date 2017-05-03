7afa0dbc2890c7dcc3b80bf43dc35d688a31ff47
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-03 16:00:27 -0700

    Pseudocode column-summing function

--- public/snippets/decompose-function.js
+++ public/snippets/decompose-function.js
@@ -1,11 +1,20 @@
 const renderColumnSums = function() {
-  // your pseudocode here
+  // for each col index up to the num of cols - 1
+  // render new cell at ColIndex, SumRow
+  // with value of computeSum for the column
 };
 
-const TODO = function() {
-  // your pseudocode here
+const computeSum = function() {
+  // sum colNumericValues
 };
 
-const TODO = function() {
-  // your pseudocode here
+const createNumericArrayFromColumn = function() {
+  // for each row index (cell) within given column
+  // test whether cell value is numeric
+  // if numeric, push cell value to array
+  // return colNumericValues (array)
 };
+
+
+// Could also set bottom function to simply aggregate the values
+// then set the middle function to filter and then sum

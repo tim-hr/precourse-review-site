cf0db376825d3fd302b94db9e0824272e2e4fbb2
soo-park <sparkstacks@gmail.com>
2017-05-04 09:38:29 -0700

    connect-four added

--- public/snippets/connect-four.js
+++ public/snippets/connect-four.js
@@ -6,8 +6,14 @@ const board = {
 const numCols = 7;
 const numRows = 6;
 
+renderBoard();
+
 const placePiece = function(player, col, row) {
   // your code here
+  let table = document.querySelector('TBODY');
+  let currentRow = table.getElementsByTagName('TR')[row];
+  currentRow.getElementsByTagName('TD')[col].innerHTML = player;
+
 };
 
 placePiece('O', 2, 5);
@@ -16,4 +22,4 @@ placePiece('O', 3, 5);
 placePiece('X', 0, 3);
 placePiece('O', 4, 5);
 
-renderBoard(); // note: this function has been provided for you
+// renderBoard(); // note: this function has been provided for you

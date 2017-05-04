e9cc03ab88454934ab682f53c31fbbb671952851
Mason Raasch <raasch.mason@gmail.com>
2017-05-03 19:08:11 -0700

    implemented the placePiece() function in connect-four.js

--- public/snippets/connect-four.js
+++ public/snippets/connect-four.js
@@ -8,6 +8,10 @@ const numRows = 6;
 
 const placePiece = function(player, col, row) {
   // your code here
+  const colChar = col.toString();
+  const rowChar = row.toString();
+  const coords = `(${colChar},${rowChar})`;
+  board[coords] = player;
 };
 
 placePiece('O', 2, 5);
@@ -15,5 +19,7 @@ placePiece('X', 0, 4);
 placePiece('O', 3, 5);
 placePiece('X', 0, 3);
 placePiece('O', 4, 5);
+
+console.log(JSON.stringify(board));
 
 renderBoard(); // note: this function has been provided for you

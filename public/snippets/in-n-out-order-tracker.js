0576eb650759e98f9d207aa81d7115da3228700c
Jennifer Wang <jw775@cornell.edu>
2017-05-04 13:39:37 -0400

    implement todos of classes topic

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,12 +1,34 @@
 class Order {
+	constructor() {
+		this.state = 1;
+	}
 
+	advanceState() {
+		if (this.state !== 4) {
+			this.state += 1;
+		}
+	}
+
+	getState() {
+		if (this.state === 1) {
+			return 'taking order';
+		} else if (this.state === 2) {
+			return 'cooking order';
+		} else if (this.state === 3) {
+			return 'requesting pickup of order';
+		} else {
+			return 'order complete'; 
+		}
+	}
 }
 
-// create order instance #1
-// create order instance #2
-// advance state of #1
-// advance state of #1
-// advance state of #2
-// advance state of #1
-// print state of #1 to console
-// print state of #2 to console
+const order1 = new Order();
+const order2 = new Order();
+
+order1.advanceState();
+order1.advanceState();
+order2.advanceState();
+order1.advanceState();
+
+console.log(order1.getState());
+console.log(order2.getState());

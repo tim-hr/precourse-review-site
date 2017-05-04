cb75cec67dba9a165c8479346f9d3a409393ec78
Derek Johnson <drk.w.jhnsn@gmail.com>
2017-05-03 23:41:02 -0500

    Hookup pony button and implement pony swapping functionality

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,8 @@
-// your code here
+const ponySwapButton = document.getElementById('btn-cycle-ponies');
+ponySwapButton.addEventListener('click', clickHandler);
+function clickHandler(evt) {
+  const ponies = document.getElementsByClassName('my-little-pony');
+  const temp = ponies[0].innerHTML;
+  ponies[0].innerHTML = ponies[1].innerHTML;
+  ponies[1].innerHTML = temp;
+}
--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,7 +1,7 @@
-const button = document.getElementById('btn-nuke-first-row');
+const removeRowButton = document.getElementById('btn-nuke-first-row');
 const tableBody = document.querySelector('TBODY');
 
-button.addEventListener('click', clickHandler.bind(this));
+removeRowButton.addEventListener('click', clickHandler.bind(this));
 
 function clickHandler(evt) {
   tableBody.removeChild(document.querySelector('TBODY TR'));

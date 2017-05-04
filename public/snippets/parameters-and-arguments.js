9f03bb98f75ad95e5bd3398e2e675882f777ae8e
Wesley Choi <weschoi.creative@gmail.com>
2017-05-04 16:46:38 -0700

    function basics: decompose and make table header row green

--- public/snippets/parameters-and-arguments.js
+++ public/snippets/parameters-and-arguments.js
@@ -1,5 +1,7 @@
+const tableHeader = document.querySelector('THEAD TR');
+
 const greenify = function(el) {
   el.setAttribute('style', 'background-color: green');
 };
 
-greenify();
+greenify(tableHeader);

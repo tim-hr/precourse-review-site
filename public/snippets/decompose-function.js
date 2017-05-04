4e69b45504c0739f05fe8d6e7c71330dfedbf8a4
Nick Akey <nick@nickakey.com>
2017-05-03 20:28:10 -0700

    completed function basics

--- public/snippets/decompose-function.js
+++ public/snippets/decompose-function.js
@@ -1,11 +1,17 @@
 const renderColumnSums = function() {
-  // your pseudocode here
+  // call renderFooterElements
 };
 
-const TODO = function() {
-  // your pseudocode here
+const calculateColumnSum = function(column) {
+  // fetch all the cell values for column from the model
+  // filter out the non-numeric values from this list
+  // add up the filtered values in the list
+  //   return the sum of the filtered values
 };
 
-const TODO = function() {
-  // your pseudocode here
+const renderFooterElements = function() {
+  // for each col index up to the num of cols
+     // find the footer element for this column
+     // set its text content to calculateColumnSum(column)
+  
 };

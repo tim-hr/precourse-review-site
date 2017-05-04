72e260683694fb12111a00fee366a3fab16e9418
Michael Clausen <michaeljclausen@gmail.com>
2017-05-03 17:43:24 -0700

    complete selectonator 5000

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,8 @@
-// your code here
+let nuke = function () {
+  let row = document.querySelector('TBODY TR');
+  row.remove(); 
+}
+
+let button = document.getElementById('btn-nuke-first-row');
+button.onclick = nuke;
+

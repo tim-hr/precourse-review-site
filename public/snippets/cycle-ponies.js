28b97a9c755efea00743ca41844b7a98bfe7212d
Nick Akey <nick@nickakey.com>
2017-05-04 11:08:53 -0700

    finished the DOM section

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,22 @@
-// your code here
+
+//I didn't really know how to do this... So I basically googled for 45 minutes 
+//and made this janky mess. Hopefully it will at least give you a good idea
+//of where I am when it comes to manipulating DOM
+
+function cyclePonies() {
+
+  const topPony = document.getElementsByTagName("img")[0];
+  const bottomPony = document.getElementsByTagName("img")[1];
+  const topPonySrc = topPony.src
+  const bottomPonySrc = bottomPony.src
+
+  const topPonyName = document.getElementsByClassName("my-little-pony")[0].childNodes[1].innerHTML;
+  const bottomPonyName = document.getElementsByClassName("my-little-pony")[1].childNodes[1].innerHTML;
+
+  document.getElementsByClassName("my-little-pony")[0].childNodes[1].innerHTML = bottomPonyName;
+  document.getElementsByClassName("my-little-pony")[1].childNodes[1].innerHTML = topPonyName;
+
+
+  topPony.src = bottomPonySrc;
+  bottomPony.src = topPonySrc;
+} 
--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,9 @@
-// your code here
+function removeTopRow() {
+  const topRow = document.querySelector('TBODY TR');
+  console.log(topRow)
+  topRow.remove();
+}
+
+// function hello() {
+//   console.log('hello!');
+// }

2881ff168c4790de8c71371e74703a465bb94b6d
Jon Anderson <janderson215@gmail.com>
2017-05-04 12:56:12 -0700

    Completed Function -> Function Basics

--- public/snippets/decompose-function.js
+++ public/snippets/decompose-function.js
@@ -1,11 +1,17 @@
-const renderColumnSums = function() {
-  // your pseudocode here
+const renderColumnSums = function(colNum) {
+  // pull value of all cells in a column into `values` array
+  // pass `values` array into addValues
+  // find footer element of column
+  // set textContent = sum
 };
 
-const TODO = function() {
-  // your pseudocode here
+const addValues = function(values) {
+  // for each el, pass el to `isNumeric` to determine if numeric
+  // if so, add to running sum
+  // return sum
 };
 
-const TODO = function() {
-  // your pseudocode here
+const isNumeric = function(value) {
+  // if parseInt(value) !== NaN, return true
 };
+
--- public/snippets/parameters-and-arguments.js
+++ public/snippets/parameters-and-arguments.js
@@ -2,4 +2,5 @@ const greenify = function(el) {
   el.setAttribute('style', 'background-color: green');
 };
 
-greenify();
+greenify(document.querySelectorAll('table thead tr th')[0]);
+greenify(document.querySelectorAll('table thead tr th')[1]);

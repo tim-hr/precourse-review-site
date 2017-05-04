e7744871a51dd204c598434555bd9745026224b4
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-04 08:16:01 -0700

    Add command lines for reworking directory tree

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,15 @@
-# TODO: list your commands here, one per line.
+rm bundle.js
+mkdir -p client/js/test
+mkdir -p public/js
+mkdir public/css
 
+mv client/test_multiplier.js client/js/test/multiplier.test.js
+mv client/app.js client/js
+mv client/bundle.js public/js
+mv server/server.js ./index.js
+rmdir server
+mv client/styles.css public/css
+mv client/index.html public
+
+#I think the below was intended but doesn't match the "After"
+mv client/multiplier.js client/js

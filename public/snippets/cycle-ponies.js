e2114a50a1ceb791e2b11861e9ea8e971c66a1ed
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-04 12:23:37 -0700

    completed half of selectors

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,20 @@
 // your code here
+
+// not sure how to resolve having two ViewManagers
+
+class ViewManager {
+	connectEventHandlers() {
+		document.getElementById('btn-cycle-ponies')
+			.addEventListener(
+				'click',
+				this.swap.bind(this));
+	}
+
+	swap(event) {
+		event.preventDefault();
+		let el = document.querySelectorAll('div.my-little-pony');
+	}
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,19 @@
 // your code here
+
+class ViewManager {
+	connectEventHandlers() {
+		document.getElementById('btn-nuke-first-row') 
+			.addEventListener(
+				'click',
+				this.nuke.bind(this));
+	}
+
+	nuke(event) {
+		event.preventDefault();
+		let el = document.querySelector('table');
+		el.deleteRow(1);
+	}
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();

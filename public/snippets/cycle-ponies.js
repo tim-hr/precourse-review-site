2f2a588b52269c1e4f5a220b47d3c8ca6f2d4c7a
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-04 16:27:43 -0700

    Add code to swap positions of ponies

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,21 @@
-// your code here
+class PonyViewManager {
+  connectEventHandlers() {
+    document.getElementById('btn-cycle-ponies')
+      .addEventListener('click',
+        this.onClick.bind(this));
+  }
+
+  onClick(event) {
+    event.preventDefault();
+    var ponyEls = document.querySelectorAll('.my-little-pony');
+
+    let ponyOne = ponyEls[0].innerHTML;
+    let ponyTwo = ponyEls[1].innerHTML;
+
+    ponyEls[0].innerHTML = ponyTwo;
+    ponyEls[1].innerHTML = ponyOne;
+  }
+}
+
+const ponyViewManager = new PonyViewManager();
+ponyViewManager.connectEventHandlers();

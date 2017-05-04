4f0a291a9e96961f6983811aab776bfc2e6341e6
Mason Raasch <raasch.mason@gmail.com>
2017-05-04 13:52:05 -0700

    translated object definition syntax to class syntax for the doggo. translated class syntax to object definition syntax for the panda assassin.

--- public/snippets/class-syntax2.js
+++ public/snippets/class-syntax2.js
@@ -1,3 +1,17 @@
-class Dog {
-  // TODO
+class Dog {'
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  listen(humanSpeech) {
+    if (humanSpeech.indexOf(this.name) !== -1) {
+      return this.bark();
+    }
+  }
+
+  bark() {
+    return 'ruff!';
+  }
+
 }

4c084306064fda2a27025c5ee000dbee759e3d66
Derek Johnson <drk.w.jhnsn@gmail.com>
2017-05-03 21:56:17 -0500

    implement in-n-out Order class

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,12 +1,31 @@
 class Order {
+  constructor() {
+    this.current = 0;
+  }
 
+  advanceState() {
+    if (this.current < this.states.length) {
+      this.current++;
+    }
+  }
+
+  getState() {
+    return this.states[this.current];
+  }
 }
 
-// create order instance #1
-// create order instance #2
-// advance state of #1
-// advance state of #1
-// advance state of #2
-// advance state of #1
-// print state of #1 to console
-// print state of #2 to console
+Order.prototype.states = [
+  'Taking order',
+  'Cooking order',
+  'Requesting pickup of order',
+  'Order complete'
+]
+
+const order1 = new Order();
+const order2 = new Order();
+order1.advanceState();
+order1.advanceState();
+order2.advanceState();
+order1.advanceState();
+console.log(order1.getState());
+console.log(order2.getState());

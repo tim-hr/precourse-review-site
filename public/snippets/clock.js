b0e7fec03e2693677061e33e12f80b8e13ee242a
soo-park <sparkstacks@gmail.com>
2017-05-04 10:13:47 -0700

    clock added

--- public/snippets/clock.js
+++ public/snippets/clock.js
@@ -1,3 +1,5 @@
+const date = new Date();
+
 const clock = {
   getHours: null,
   getMinutes: null,
@@ -5,3 +7,14 @@ const clock = {
 };
 
 
+function digitalClock(date) {
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    document.getElementsByTagName('span')[0].innerHTML = h;
+    document.getElementsByTagName('span')[1].innerHTML = m;
+    document.getElementsByTagName('span')[2].innerHTML = s;
+}
+
+digitalClock(date);

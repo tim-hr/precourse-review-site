64e281132e877c517babfa8379bc4a313bde6f33
Michael Chiang <chiangshtaa@gmail.com>
2017-05-03 22:13:36 -0700

    add delete row button functionality

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,7 @@
-// your code here
+const deleteRow = function() {
+  const row = document.querySelector('tbody tr');
+  row.remove();
+}
+
+deleteRowButton = document.getElementById('btn-nuke-first-row');
+deleteRowButton.addEventListener('click', deleteRow);

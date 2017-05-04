3044578039e220ad8ff6a1b8e0ce860b399e08e8
Nick Akey <nick@nickakey.com>
2017-05-04 10:08:47 -0700

    completed the command line section

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,21 @@
-# TODO: list your commands here, one per line.
+' Off the top of my head, I do not know how to do move files. 
+That being the case, I spent 5 minutes googling to find out.'
+
+cd client
+mkdir js
+mv app.js js
+cd js
+mkdir test
+cd .. 
+mv "multiplier.js" "multiplier.test.js"
+mv multiplier.test.js js/test
+cd ..
+mkdir public
+mkdir public/css
+mkdir public/js
+mv client/styles.css public/css
+mv client/bundle.js public/js
+mv index.html public
+
+
 

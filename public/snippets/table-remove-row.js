3e2ab25971e427167c1a359a43fa8ced523f5e5a
Jerizon <JesseLowellReyes@gmail.com>
2017-05-04 08:19:36 -0700

    "Remove top row of data using Nuke the first data row button"

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,6 +1,6 @@
  initDomReferences = function() {
      const firstRowElements = document.querySelectorAll('TBODY TR TD');
-     const nukeButton = document.querySelector("#btn-nuke-first-row");
+     const nukeButton = document.querySelector('#btn-nuke-first-row');
      //console.log(firstRowElements);
      attachEventHandlers = function() {
          nukeButton.addEventListener('click', emptyFirstRow);

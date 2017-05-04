0ccacd4dac13b962ed9aab82bf08456fd9b9435f
Andrea Miralles <andreamiralles@andreamllessmbp.attlocal.net>
2017-05-04 15:24:59 -0700

    command line

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,12 @@
-# TODO: list your commands here, one per line.
+mkdir -p client/js/test
+mv client/app.js client/js
+mv client/test_multiplier.js client/js/test/multiplier.test.js
+mv server public
+mv public/server.js index.js
+mkdir public/css
+touch public/css/styles.css
+mv client/index.html public
+mkdir public/js
+touch public/js/bundle.js
+
 

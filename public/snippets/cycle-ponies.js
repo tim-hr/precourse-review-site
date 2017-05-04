c4b4b07ea7360e350785035c3c564af1a2ef526d
Jerizon <JesseLowellReyes@gmail.com>
2017-05-04 08:20:27 -0700

    "Swap Fluttershy and Applejack with the swap ponies button."

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,16 @@
-// your code here
+initDomPonies = function() {
+    var ponies = document.querySelectorAll('.my-little-pony');
+    const swapPonyButton = document.querySelector('#btn-cycle-ponies');
+    
+    attachEventHandlers = function() {
+        swapPonyButton.addEventListener('click', swapPonies);
+    }
+   
+    swapPonies = function() {
+        ponies[0].before(ponies[1]);
+        ponies = document.querySelectorAll('.my-little-pony');
+    }
+}
+
+initDomPonies();
+attachEventHandlers();

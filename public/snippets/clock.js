c4668c5832700d1a64f068a5de0f2f35bc7a38c1
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-04 08:54:33 -0700

    Add clock methods

--- public/snippets/clock.js
+++ public/snippets/clock.js
@@ -1,7 +1,14 @@
 const clock = {
-  getHours: null,
-  getMinutes: null,
-  getSeconds: null
+  getHours: function() {
+    let timeStamp = new Date(Date.now());
+    return timeStamp.getHours();
+  },
+  getMinutes: function() {
+    let timeStamp = new Date(Date.now());
+    return timeStamp.getMinutes();
+  },
+  getSeconds: function() {
+    let timeStamp = new Date(Date.now());
+    return timeStamp.getSeconds();
+  }
 };
-
-

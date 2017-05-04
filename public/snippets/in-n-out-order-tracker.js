0f64dad155e98d2b4769afae868dd39631ec5811
Jon Anderson <janderson215@gmail.com>
2017-05-04 13:44:52 -0700

    Completed Classes -> Classes are bundles of data and behavior

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,4 +1,19 @@
 class Order {
+  constructor() {
+    this.state = 'ordered';
+  }
+
+  cooking() {
+    this.state = 'cooking';
+  }
+
+  requestingPickup() {
+    this.state = 'requesting pickup';
+  }
+
+  completeOrder() {
+    this.state = 'completed order';
+  }
 
 }
 
@@ -10,3 +25,12 @@ class Order {
 // advance state of #1
 // print state of #1 to console
 // print state of #2 to console
+
+let order1 = new Order();
+let order2 = new Order();
+order1.cooking();
+order1.requestingPickup();
+order2.cooking();
+order1.completeOrder();
+console.log(order1.state);
+console.log(order2.state);

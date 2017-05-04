d948a42fdd895256636cba93658880bb8adcd3c2
Jerizon <JesseLowellReyes@gmail.com>
2017-05-03 22:29:38 -0700

    "Remove exclusively the first row in Selectinator 5000"

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,17 @@
-// your code here
+ initDomReferences = function() {
+     const firstRowElements = document.querySelectorAll('TBODY TR TD');
+     const nukeButton = document.querySelector("#btn-nuke-first-row");
+     //console.log(firstRowElements);
+     attachEventHandlers = function() {
+         nukeButton.addEventListener('click', emptyFirstRow);
+     }
+
+     emptyFirstRow = function() {
+         for (i = 0; i < 3; i++) {
+         	firstRowElements[i].textContent = "";
+         }
+     }
+ }
+ 
+ initDomReferences();
+ attachEventHandlers();

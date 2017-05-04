d7d396ee8dc791e82a37b989d0f405033356a780
Mason Raasch <raasch.mason@gmail.com>
2017-05-04 13:05:14 -0700

    implemented the methods for the clock class

--- public/snippets/clock.js
+++ public/snippets/clock.js
@@ -1,7 +1,21 @@
-const clock = {
-  getHours: null,
-  getMinutes: null,
-  getSeconds: null
-};
+const getHours = function() {
+	const date = new Date();
+	return date.getHours();
+}
 
+const getMinutes = function() {
+	const date = new Date();
+	return date.getMinutes();
+}
+
+const getSeconds = function() {
+	const date = new Date();
+	return date.getSeconds();
+}
+
+const clock = {
+  getHours: getHours,
+  getMinutes: getMinutes,
+  getSeconds: getSeconds
+};
 

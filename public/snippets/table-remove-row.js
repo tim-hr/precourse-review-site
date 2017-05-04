094b94e91772ae42193eff49e34e498c82d5c071
Mason Raasch <raasch.mason@gmail.com>
2017-05-04 15:28:35 -0700

    implemented functionality for the nuke the first data row button

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,11 @@
 // your code here
+const getFirstTableRow = function() {
+  return document.querySelectorAll('TBODY TR')[0];
+}
+const nukeFirstRow = function() {
+  const table = document.querySelector('TBODY')
+  const firstRow = getFirstTableRow();
+  table.removeChild(firstRow);
+}
+document.getElementById('btn-nuke-first-row').addEventListener('click', 
+  nukeFirstRow);

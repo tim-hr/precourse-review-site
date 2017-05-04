b09d547e4ee1ced5799a25fc024f66e14aba7a3b
Mason Raasch <raasch.mason@gmail.com>
2017-05-04 13:44:29 -0700

    implemented factory function that creates greeting functions

--- public/snippets/greeting-factory-function.js
+++ public/snippets/greeting-factory-function.js
@@ -1,13 +1,16 @@
 const makeGreetingFunction = function(greeting) {
-  // TODO
-};
-
-const greet1 = makeGreetingFunction('Salutations');
-
-greet1('Bilbo'); // "Salutations, Bilbo!"
-greet1('Saruman'); // "Salutations, Saruman!"
-
-const greet2 = makeGreetingFunction('Hola');
-
-greet2('Hermione'); // "Hola, Hermione!"
-greet2('Voldemort'); // "Hola, Voldemort!"
+  return function(name) {
+      return `${greeting}, ${name}!`
+    };
+  };
+  
+  const greet1 = makeGreetingFunction('Salutations');
+  
+  greet1('Bilbo'); // "Salutations, Bilbo!"
+  greet1('Saruman'); // "Salutations, Saruman!"
+  
+  const greet2 = makeGreetingFunction('Hola');
+  
+  greet2('Hermione'); // "Hola, Hermione!"
+  greet2('Voldemort'); // "Hola, Voldemort!"
+  

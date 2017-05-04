cbf1677903b314c91722312e079249f9a2be8549
Kirk Rohani <Kirk Rohani>
2017-05-04 13:42:52 -0700

    completed self-intro and deeply-nested-objects

--- public/snippets/deeply-nested-object.js
+++ public/snippets/deeply-nested-object.js
@@ -14,5 +14,18 @@ const nestedObj = {
   }
 };
 
-const nestedObjValue = 'replace me with the innermost value above';
+// const nestedObj = {
+//     'quuuuux!': 'you got me'
+  
+// };
+
+const innerMostValue = function(obj) {
+  const value =  obj[Object.keys(obj)];
+  if (typeof value === "string")
+    return value;
+  
+  return innerMostValue(value);
+};
+
+const nestedObjValue = innerMostValue(nestedObj);
 document.write(`<h4>${nestedObjValue}</h4>`);

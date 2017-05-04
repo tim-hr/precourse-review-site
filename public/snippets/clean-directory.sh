54a504d3436c09aa4aeafa15b0e994eeef0268dd
soo-park <sparkstacks@gmail.com>
2017-05-04 16:18:08 -0700

    in-n-out, command line 1&2 added

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,17 @@
-# TODO: list your commands here, one per line.
+mkdir public
+mkdir public/css
+mkdir public/js
+mkdir client/js
+mkdir client/js/test/
 
+rm bundle.js
+
+mv ./client/*.* ./
+rm multiplier.js
+rm -rf server
+
+mv app.js ./client/js/
+mv bundle.js ./public/js/
+mv index.html ./public
+mv styles.css ./public/css/
+mv test_multiplier.js ./client/js/test/muliplier.test.js
--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,12 +1,44 @@
-class Order {
+const state = {
+  1: "Taking order",
+  2: "Cooking order",
+  3: "Requesting pickup of order",
+  4: "Order complete"
+};
 
+class Order {
+  constructor(state=1) {
+    this.state = {state};
+  }
+
+  advanceState() {
+    this.state += 1;
+  }
+
+  printState() {
+    console.log(this.state);
+  }
 }
 
 // create order instance #1
+order1 = Order();
+
 // create order instance #2
+order2 = Order();
+
 // advance state of #1
+order1.advanceState();
+
 // advance state of #1
+order1.advanceState();
+
 // advance state of #2
+order2.advanceState();
+
 // advance state of #1
+order2.advanceState();
+
 // print state of #1 to console
+order1.printState();
+
 // print state of #2 to console
+order2.printState();

b98c5a3d0a4b9a35f6e63efd7dd67dcd9f147d86
soo-park <sparkstacks@gmail.com>
2017-05-04 14:58:48 -0700

    greeting factory function added

--- public/snippets/parameters-and-arguments.js
+++ public/snippets/parameters-and-arguments.js
@@ -2,4 +2,7 @@ const greenify = function(el) {
   el.setAttribute('style', 'background-color: green');
 };
 
-greenify();
+let table = document.getElementsByTagName("table")[1]
+let headRow = table.getElementsByTagName('tr')[0]
+
+greenify(headRow);

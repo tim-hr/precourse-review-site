277c3eaa09bb34849bffc3780221ac3a093280f2
laraismael1 <laraismael123@gmail.com>
2017-05-04 10:13:25 -0700

    function-basic pseudocoding -decompose function into single perpose functions

--- public/snippets/decompose-function.js
+++ public/snippets/decompose-function.js
@@ -1,11 +1,14 @@
 const renderColumnSums = function() {
-  // your pseudocode here
+  // find the footer element for this column
+  // set its text content to the sum
 };
 
-const TODO = function() {
-  // your pseudocode here
+const filterNum = function() {
+  // filter out the non-numeric values from this list
+  // add up the filtered values in the list
 };
 
-const TODO = function() {
-  // your pseudocode here
+const fatchCell = function() {
+  // for each col index up to the num of cols
+  // fetch all the cell values for that column from the model
 };

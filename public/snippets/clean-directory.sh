7ff6078c04a05e0c5d6b7062671b2504d6d719ec
Derek Johnson <drk.w.jhnsn@gmail.com>
2017-05-03 22:38:08 -0500

    Reorganize sample directory

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,15 @@
-# TODO: list your commands here, one per line.
-
+rm bundle.js
+mkdir client/js
+mkdir client/js/test
+mkdir public
+mkdir public/css
+mkdir public/js
+cd client
+mv app.js js
+mv test_multiplier.js js/test
+cd ..
+mv server/server.js index.js
+rmdir server
+mv client/styles.css public/css
+mv client/index.html public
+mv client/bundle.js public/js
--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,8 @@
-// your code here
+const button = document.getElementById('btn-nuke-first-row');
+const tableBody = document.querySelector('TBODY');
+
+button.addEventListener('click', clickHandler.bind(this));
+
+function clickHandler(evt) {
+  tableBody.removeChild(document.querySelector('TBODY TR'));
+}

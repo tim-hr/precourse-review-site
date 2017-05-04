6ce9eb0253194aa5b97369cad2309c50fb2b7640
Mason Raasch <raasch.mason@gmail.com>
2017-05-04 14:53:02 -0700

    removed comments from shell commands (assuming you were going to auto test those somewhow) and patched the selectors.html file by adding in the necessary script tag for cycling ponies

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,11 @@
 # TODO: list your commands here, one per line.
-
+mkdir -p client/js/test
+mv client/app.js client/js/
+mv client/test_multiplier.js client/js/test/multiplier.test.js
+mkdir -p public/css
+mv client/styles.css public/css/
+mkdir public/js
+mv bundle.js public/js/
+mv client/index.html public/
+mv server/server.js ./index.js
+rm server

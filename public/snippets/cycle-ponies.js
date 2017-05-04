f58716ddf694c0ce6ca58964b491a91217c6061b
Jon Anderson <janderson215@gmail.com>
2017-05-04 16:50:58 -0700

    Completed The DOM -> Selectors

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,10 @@
-// your code here
+const swapPositions = function() {
+  const wrapper = document.querySelector('.wrapper');
+  const topPony = document.querySelectorAll('.my-little-pony')[0];
+  const bottomPony = document.querySelectorAll('.my-little-pony')[1];
+  wrapper.removeChild(bottomPony)
+  wrapper.insertBefore(bottomPony, topPony)
+}
+
+document.getElementById("btn-cycle-ponies")
+  .addEventListener("click", swapPositions);
--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,14 @@
-// your code here
+const tableBody = document.querySelector("tbody");
+let topRow = document.querySelectorAll("tbody tr");
+
+const removeTopRow = function() {
+  if (topRow.length === 0) {
+    alert('No more data rows, bro!');
+  } else {
+    tableBody.removeChild(topRow[0]);
+    topRow = document.querySelectorAll("tbody tr");
+  }
+}
+
+document.getElementById("btn-nuke-first-row")
+  .addEventListener("click", removeTopRow);

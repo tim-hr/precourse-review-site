cc33c920054b6f76ca2063e42373e7af0aa459cd
Paul Dockery <paul.david.dockery@gmail.com>
2017-05-04 15:37:42 -0700

    Implement nuking first row of data table with button press

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,20 @@
-// your code here
+class ViewManager {
+  
+  connectEventHandlers() {
+    document.getElementById('btn-nuke-first-row')
+      .addEventListener('click',
+        this.onClick.bind(this));
+  }
+
+  onClick(event) {
+    console.log('test');
+    event.preventDefault();
+    var bodyEl = document.querySelector('TBODY');
+    bodyEl.removeChild(bodyEl.firstChild); // remove blank text node
+    bodyEl.removeChild(bodyEl.firstChild); // remove actual row node
+  }
+
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();

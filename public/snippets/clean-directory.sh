5ad877f791df16a3c1572272c6806e66abfb3f69
Jon Anderson <janderson215@gmail.com>
2017-05-04 14:45:34 -0700

    Completed The command line

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,14 @@
 # TODO: list your commands here, one per line.
 
+mkdir -p client/js/test
+mkdir -p public/js
+mkdir public/css
+rm bundle.js
+rm server/server.js
+rmdir server
+mv client/app.js client/js/
+mv client/bundle.js public/js/
+mv client/index.html public/
+mv client/multiplier.js index.js
+mv client/styles.css public/css/
+mv client/test_multiplier.js client/js/test/multiplier.test.js

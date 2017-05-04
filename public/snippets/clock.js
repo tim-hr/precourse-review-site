1680da952d416f17716983d8466552b26f95a7a8
mttrys <mttrys@gmail.com>
2017-05-03 19:27:58 -0700

    Implemented Objects Methods Clock

--- public/snippets/clock.js
+++ public/snippets/clock.js
@@ -1,14 +1,7 @@
-let date = new Date();
+const date = new Date();
 
 const clock = {
   getHours: date.getHours(),
   getMinutes: date.getMinutes(),
   getSeconds: date.getSeconds()
 };
-
-// the function gets the current time but
-// it does not render in methods.html
-
-console.log(clock.getHours)
-console.log(clock.getMinutes)
-console.log(clock.getSeconds)

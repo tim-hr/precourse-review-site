0440c428e65279b78da7ec0f3cda801c9fa3bc45
Joseph Lei <josephlei@Josephs-MacBook-Pro.local>
2017-05-05 10:33:06 -0700

    finish classes in-n-out

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,12 +1,24 @@
 class Order {
+	constructor(state) {
+		this.state = state;
+	}
 
+	advanceState() {
+		this.states = ['Taking order', 'Cooking order', 'Requesting pickup of order','Order complete'];
+		this.state = this.state +1
+		return this.states[this.state];
+	}
+
+	printState() {
+		console.log(this.states[this.state]);
+	}
 }
 
-// create order instance #1
-// create order instance #2
-// advance state of #1
-// advance state of #1
-// advance state of #2
-// advance state of #1
-// print state of #1 to console
-// print state of #2 to console
+let instance1 = new Order(0);
+let instance2 = new Order(0);
+
+instance1.advanceState();
+instance1.advanceState();
+instance2.advanceState();
+instance1.printState();
+instance2.printState();

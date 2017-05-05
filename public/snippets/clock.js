e43d3182911aae118f7381c4b2f26751c297660c
Corey <cry@Coreys-Air.attlocal.net>
2017-05-05 16:34:17 -0700

    Cleared methods and got clock working

--- public/snippets/clock.js
+++ public/snippets/clock.js
@@ -1,7 +1,19 @@
 const clock = {
-  getHours: null,
-  getMinutes: null,
-  getSeconds: null
+  getHours: function() {
+    let hrs = new Date();
+    let hours = hrs.getHours();
+    return hours;
+  },
+  getMinutes: function() {
+    let min = new Date();
+    let minutes = min.getMinutes();
+    return minutes;
+  },
+  getSeconds: function() {
+    let sec = new Date();
+    let seconds = sec.getSeconds();
+    return seconds;
+  }
 };
 
 

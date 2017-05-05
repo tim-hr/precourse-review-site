8946f2bddf2a786ec56c76de0b8dd66ab62fd11a
Henri Pietila <henrismail@gmail.com>
2017-05-05 02:41:19 -0700

    added name, fixed clock, connect four, decompose, deeply nested, parmeters and arugments

--- public/snippets/decompose-function.js
+++ public/snippets/decompose-function.js
@@ -1,11 +1,20 @@
+
+
+const calculateColumnSum = function() {
+  //  fetch all the cell values for that column from the model
+  //   filter out the non-numeric values from this list
+  //   add up the filtered values in the list
+
+};
+
+const getAllColumnSums = function() {
+  // for each col index up to the num of cols
+  //  add value from call calculateColumnSum to array
+  // return array
+};
+
 const renderColumnSums = function() {
-  // your pseudocode here
-};
+  //   find the footer element for this column
+  //   set its text content to the sums from getAllColumnSums
 
-const TODO = function() {
-  // your pseudocode here
-};
-
-const TODO = function() {
-  // your pseudocode here
 };

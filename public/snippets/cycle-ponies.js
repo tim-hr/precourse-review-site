43bee27d6dfa3a2e1f4e76e247d68d962c2f1fa8
codepadma <padma.govindarajalu@gmail.com>
2017-05-04 22:48:42 -0700

    answers to Selectinator 5000

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,29 @@
-// your code here
+class ImageHandler {
+  attachEventHandlers() {
+    document.querySelector('#btn-cycle-ponies').addEventListener('click', this.onClickSwapPositions);
+  }
+
+  onClickSwapPositions(event) {
+    const ponies = document.querySelectorAll('.my-little-pony');
+    const pony1 = ponies[0].querySelector('div');
+    const img1 = ponies[0].querySelector('img');
+
+    const pony2 = ponies[1].querySelector('div');
+    const img2 = ponies[1].querySelector('img');
+
+    const ponyName1 = pony1.innerHTML;
+    const imgSrc1 = img1.getAttribute('src');
+
+    const ponyName2 = pony2.innerHTML;
+    const imgSrc2 = img2.getAttribute('src');
+
+    pony1.innerHTML = ponyName2;
+    img1.setAttribute('src', imgSrc2);
+
+    pony2.innerHTML = ponyName1;
+    img2.setAttribute('src', imgSrc1);
+  }
+}
+
+const imageHandler = new ImageHandler();
+imageHandler.attachEventHandlers();

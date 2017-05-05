8c9daa346794eac2a1c5a607dbd6974b588e8eb5
Joseph Lei <josephlei@Josephs-MacBook-Pro.local>
2017-05-05 00:01:43 -0700

    finish up to function basics

--- public/snippets/parameters-and-arguments.js
+++ public/snippets/parameters-and-arguments.js
@@ -2,4 +2,6 @@ const greenify = function(el) {
   el.setAttribute('style', 'background-color: green');
 };
 
-greenify();
+let headerElement = document.querySelector('thead tr')
+
+greenify(headerElement);

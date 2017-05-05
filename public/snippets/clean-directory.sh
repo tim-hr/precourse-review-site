3bde911dfd12b24693ec91894a8ff1f98b912ab7
Jun Park <jun.park@live.com>
2017-05-05 11:18:05 -0600

    finished Clean Directory module, still need to add syntax for redirecting the bundle.js location in the package.json file

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,14 @@
-# TODO: list your commands here, one per line.
-
+mkdir client/js
+mkdir client/js/test
+mkdir public
+mkdir public/css
+mkdir public/js
+rm bundle.js
+mv client/test_multiplier.js client/js/test/multiplier.test.js
+mv client/app.js client/js
+mv client/bundle.js public/js
+mv client/index.html public
+mv client/styles.css public/css
+mv server/server.js index.js
+rmdir server
+rm client/multiplier.js

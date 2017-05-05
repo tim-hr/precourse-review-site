4faacc299c0307286abc311452c1dfe83d0c711a
Yermek Alzanov <yermek-alzhanov@utulsa.edu>
2017-05-05 17:41:46 -0500

    Functions review finished

--- public/snippets/decompose-function.js
+++ public/snippets/decompose-function.js
@@ -1,11 +1,28 @@
 const renderColumnSums = function() {
-  // your pseudocode here
+  // for each col index up to the num of cols
+    //fetchCellValues
+    //filterOutNonNumVal
+    //addFilteredVal
+    //findFooter
+    //setTextToSum
 };
 
-const TODO = function() {
-  // your pseudocode here
+const fetchCellValues = function() {
+  //fetch all the cell values for that column from the model
 };
 
-const TODO = function() {
-  // your pseudocode here
+const filterOutNonNumVal = function() {
+  //filter out the non-numeric values
+};
+
+const addFilteredVal = function() {
+//add up the filtered values in the list
+};
+
+const findFooter = function() {
+//find the footer element for this column
+};
+
+const setTextToSum = function() {
+//set its text content to the sum
 };

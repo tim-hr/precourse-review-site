97222a23288113cb8551dfabf02741fece494b0f
Wesley Choi <weschoi.creative@gmail.com>
2017-05-05 03:18:57 -0700

    switch position of ponies

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,15 @@
 // your code here
+
+function connectEventHandler() {
+  document.getElementById("btn-cycle-ponies")
+    .addEventListener('click', this.switchSpot.bind(this));
+}
+
+function switchSpot() {
+  let first = document.getElementsByClassName("my-little-pony")[0];
+  let second = document.getElementsByClassName("my-little-pony")[1];
+
+  first.parentNode.insertBefore(second,first);
+}
+
+connectEventHandler();

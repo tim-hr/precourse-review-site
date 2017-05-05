a87c9367dd949fa30b04b24948547c29b9b3a37c
David Vassett <david@davidvassett.com.au>
2017-05-05 15:42:58 -0700

    Completed: Function basics

--- public/snippets/decompose-function.js
+++ public/snippets/decompose-function.js
@@ -1,11 +1,14 @@
 const renderColumnSums = function() {
-  // your pseudocode here
+  // find the footer element for the column
+  // set its text content to the sum
 };
 
-const TODO = function() {
-  // your pseudocode here
+const calulateColumnSums = function() {
+  // add up the filtered values in the list
 };
 
-const TODO = function() {
-  // your pseudocode here
+const getColumnValues = function() {
+  // for each column index up to the number of columns
+  //   fetch all of the cell values for that column from the model
+  //   filter out the non-numeric values from this list
 };

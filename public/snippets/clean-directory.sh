5279e84118bcbc2e987e2ec9b25584e18d815047
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-04 19:34:01 -0700

    implement algorithm for navigating and manipulating a directory hierarchy

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,13 @@
-# TODO: list your commands here, one per line.
-
+cd client
+mkdir js
+mv app.js js/app.js
+cd js
+mkdir test
+mv test_multiplier js/test/multiplier.test.js
+touch index.js
+mkdir public
+mkdir css
+mkdir js
+mv bundle.js public/js/bundle.js
+mv client/styles.css public/css/styles.css
+rm client/multiplier.js

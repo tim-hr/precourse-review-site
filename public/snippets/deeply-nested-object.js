df6c0fd4b395b9208ced7b3d22e508dae483005b
Corey <cry@Coreys-Air.attlocal.net>
2017-05-05 15:49:45 -0700

    Cleared accessing object values

--- public/snippets/deeply-nested-object.js
+++ public/snippets/deeply-nested-object.js
@@ -14,5 +14,7 @@ const nestedObj = {
   }
 };
 
-const nestedObjValue = 'replace me with the innermost value above';
+const nestedObjValue = nestedObj['foo']['bar']['baz']['quux']['quuux']['quuuux']['quuuuux!'];
+const nestedObjValueDot = 'I don\'t think I can do this yet / it can\'t be done cleanly';
 document.write(`<h4>${nestedObjValue}</h4>`);
+document.write(`<h5>${nestedObjValueDot}</h5>`)

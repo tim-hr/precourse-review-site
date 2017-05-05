2a4ab0e8802cfb5ea045d516acb586ad90ae7ca5
LZ <luyizk@gmail.com>
2017-05-04 23:25:03 -0400

    add answers to classes and DOM sections

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,7 +1,9 @@
-const firstDivContent = document.getElementsByClassName("my-little-pony")[0].innerHTML;
-const secondDivContent = document.getElementsByClassName("my-little-pony")[1].innerHTML;
 const cyclePoniesEl = document.getElementById("btn-cycle-ponies");
 
 cyclePoniesEl.addEventListener('click', function(event) {
-	console.log('hi');
+	const firstDivContent = document.getElementsByClassName("my-little-pony")[0].innerHTML; 
+	const secondDivContent = document.getElementsByClassName("my-little-pony")[1].innerHTML;
+
+	document.getElementsByClassName("my-little-pony")[0].innerHTML = secondDivContent;
+	document.getElementsByClassName("my-little-pony")[1].innerHTML = firstDivContent;
 });
--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,12 +1,37 @@
 class Order {
+	constructor() {
+		this.stateNumber = 1;
+		this.states = { 1 : "Taking order",
+	                  2 : "Cooking order",
+	                  3 : "Requesting pickup of order",
+	                  4 : "Order complete" }
+	}
+
+	advanceOrder() {
+		if (this.stateNumber <= 3) {
+			this.stateNumber++;
+		}
+	}
+
+	getState() {
+		return this.states[this.stateNumber];
+	}
 
 }
 
 // create order instance #1
+const order1 = new Order();
 // create order instance #2
+const order2 = new Order();
 // advance state of #1
+order1.advanceOrder();
 // advance state of #1
+order1.advanceOrder();
 // advance state of #2
+order2.advanceOrder();
 // advance state of #1
+order1.advanceOrder();
 // print state of #1 to console
+console.log(order1.getState());
 // print state of #2 to console
+console.log(order2.getState());

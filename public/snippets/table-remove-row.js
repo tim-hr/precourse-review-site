84a713ec9bea2cc6639189f27470c10ab7d097c9
Wesley Choi <weschoi.creative@gmail.com>
2017-05-05 03:03:30 -0700

    manipulate tables and add script tag in the bottom of html file

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,14 @@
 // your code here
+
+function connectEventHandler() {
+  document.getElementById("btn-nuke-first-row")
+    .addEventListener('click', this.onClick.bind(this));
+}
+
+function onClick() {
+  event.preventDefault();
+  let row = document.querySelectorAll("TBODY TR")[0];
+  row.parentNode.removeChild(row);
+}
+
+connectEventHandler();

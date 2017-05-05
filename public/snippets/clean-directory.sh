50b677a8ef3d2f72517c70daf5d8faacbb6b033f
Jesus Esquivel <JAER@Jesuss-Air.PK5001Z>
2017-05-05 13:55:24 -0700

    The Command Line Section

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,45 @@
 # TODO: list your commands here, one per line.
 
+ls # Will list the current directory
+
+cd client # Change directory
+
+mkdir js  #Make a js file under the client directory
+
+cd js # Change to js directory
+
+touch app.js # Make app.js file in the js folder
+
+mkdir test # Make a test folder
+
+cd test # change directory to test folder
+
+touch multiplier.test.js # make multiplier test folder
+
+cd . # Move up a folder until you get back to the root folder
+
+touch index.js #Make a index.js file in the root folder
+
+mkdir public # Make public folder
+
+cd public # Change directory to public
+
+mkdir css # Make css directory
+
+cd css # change directory to css
+
+touch styles.css # Make styles.css folder
+
+cd . # move up a folder to root folder
+
+mv index.html public # move the index.html file to the public directory
+
+cd public #change directory to the publc folder
+
+mkdir js #create directory in public file
+
+cd . #move up to the folder to the root folder
+
+mv bundle.js public/js #move the bundle.js to the public/js diretory
+
+tree #look at the tree of all changes

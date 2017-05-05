2b78c9353eb0a8acd528a32ac0df886188913789
Mason Raasch <raasch.mason@gmail.com>
2017-05-04 18:04:19 -0700

    implemented pony cycling function with builtin method insertBefore()

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,11 @@
 // your code here
+let ponies = document.querySelectorAll('.my-little-pony');
+const swapPonies = function() {
+  const ponyContainer = document.querySelector('.pony-container');
+  ponyContainer.insertBefore(ponies[1], ponies[0]);
+  ponies = document.querySelectorAll('.my-little-pony');
+};
+
+
+document.getElementById('btn-cycle-ponies').addEventListener('click', swapPonies);
+

4af3ce5d5ac776a70a1224eaa930f76f7d1cd0f8
David Oh <davidjohis@gmail.com>
2017-05-04 21:50:10 -0700

    complete command line - navigating and manipulating a directory hierarchy

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,13 @@
 # TODO: list your commands here, one per line.
 
+mkdir client/js/test
+mkdir public/css
+mkdir public/js
+
+mv bundle.js public/js
+cat index.js
+mv client/index.html public
+mv client/styles.css public/css
+mv client/test_multiplier.js multiplier.test.js
+mv client/multiplier.test.js client/js/test
+mv client/app.js client/js

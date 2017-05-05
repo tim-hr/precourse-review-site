435ea06645acf0a7638d0fdc9041272ad00287e8
Yermek Alzanov <yermek-alzhanov@utulsa.edu>
2017-05-05 14:52:18 -0500

    Objects review finished

--- public/snippets/clock.js
+++ public/snippets/clock.js
@@ -1,7 +1,18 @@
 const clock = {
-  getHours: null,
-  getMinutes: null,
-  getSeconds: null
+  getHours: function(){
+    let date = new Date();
+    return date.getHours()
+  },
+  getMinutes: function(){
+    let date = new Date();
+    return date.getMinutes()
+  },
+  getSeconds: function(){
+    let date = new Date();
+    return date.getSeconds()
+  }
 };
+
+
 
 

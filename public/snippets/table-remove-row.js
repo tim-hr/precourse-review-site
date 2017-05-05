0fb20cf8a718a344e9e847d90f7e847f599fb04a
ststar8623 <ststar8623@gmail.com>
2017-05-05 00:56:25 -0700

    complete Topic: The DOM

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,11 @@
 // your code here
+const btnNuke = document.getElementById('btn-nuke-first-row');
+
+btnNuke.addEventListener('click', function(){
+  const trs = document.querySelector('TBODY TR');
+
+  trs.childNodes[3].innerHTML = '';
+  trs.childNodes[5].innerHTML = '';
+  trs.childNodes[7].innerHTML = '';
+  
+});

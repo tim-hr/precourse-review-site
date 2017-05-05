15769d0f3bd1baac9901874486cb2a5ef8569393
Jeffrey <jeffrey.d.sun@gmail.com>
2017-05-04 22:13:44 -0700

    add clean-directory

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,22 @@
 # TODO: list your commands here, one per line.
+vim .gitignore
+  i: bundle.js
+  esc
+  :wq
+
+
+mkdir client/js
+mv client/app.js client/js
+mkdir client/js/test
+mv test_multiplier.js client/js/test/multiplier.test.js
+
+mkdir public
+mkdir public/js
+mkdir public/css
+
+mv client/bundle.js public/js
+mv client/index.html public
+mv client/style.css public/css
+touch index.js
+
 

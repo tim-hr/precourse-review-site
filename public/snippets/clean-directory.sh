54ff6ae70f3e35205f910fb312416bb750be2329
Joseph Lei <josephlei@Josephs-MacBook-Pro.local>
2017-05-05 11:04:33 -0700

    finish navigating dir

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,16 @@
 # TODO: list your commands here, one per line.
 
+mkdir public
+mkdir client/js
+mkdir client/js/test
+mkdir public
+mkdir public/css
+mkdir public/js
+mv client/app.js client/js/app.js
+mv client/multiplier.js client/js/test/multiplier.test.js
+mv client/index.html public
+mv client/styles.css public/css/styles.css
+mv bundle.js public/js/bundle.js
+rm client/bundle.js
+touch index.js
+rm client/test_multiplier.js

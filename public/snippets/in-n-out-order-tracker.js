0317c5d19350f219eb72c360c1f245706bce9218
Taras Ignashchenko <tignashchenko@gmail.com>
2017-05-04 20:58:41 -0700

    implement In n Out class

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,12 +1,45 @@
 class Order {
+  constructor() {
+    this.data = {};
+  }
 
+  takeOrder(tableNumber) {
+    this.data[tableNumber] = 'Order received!';
+    return this.data[tableNumber];
+  }
+
+  cookOrder(tableNumber) {
+    if(this.data[tableNumber] === 'Order received!') {
+      this.data[tableNumber] = 'Order is being cooked!';
+    } else {
+      this.data[tableNumber] = 'Please make order!';
+    }
+    if(this.data[tableNumber] = 'Order is being cooked!') {
+      this.data[tableNumber] = 'Order is ready!';
+    }
+    return this.data[tableNumber];
+  }
+
+  requestOrderPickup(tableNumber) {
+    if(this.data[tableNumber] === 'Order is ready!') {
+      this.data[tableNumber] = 'Taking order to table!';
+    } else {
+      this.data[tableNumber] = 'Order is not ready!';
+    }
+    if(this.data[tableNumber] === 'Taking order to table!') {
+      this.data[tableNumber] = 'Order is complete!';
+    }
+    return this.data[tableNumber];
+  }
 }
 
-// create order instance #1
-// create order instance #2
-// advance state of #1
-// advance state of #1
-// advance state of #2
-// advance state of #1
-// print state of #1 to console
-// print state of #2 to console
+
+const order1 = new Order();
+const order2 = new Order();
+
+order1.takeOrder(1);
+order1.cookOrder(1);
+order2.takeOrder(2);
+order1.requestOrderPickup(1);
+console.log(order1.data['1']);
+console.log(order2.data['2']); 

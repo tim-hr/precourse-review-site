4b3eb2d9c57f0e245256b1ad694d7c1e2f777587
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-04 19:58:53 -0700

    actually finished selector portion YESSSSSSS

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -6,11 +6,20 @@ class SwapPonies {
         this.swap.bind(this));
   }
 
-  swap(event) { // HALP
+  swap(event) { // I DID IT THANKS TIM!!!! 
     event.preventDefault();
-    let el = document.getElementsByClassName('my-little-pony');
+    let el = document.querySelectorAll('.my-little-pony div');
     
-    // couldn't figure it out :(
+    let tempText = el[0].textContent;
+    el[0].textContent = el[1].textContent;
+    el[1].textContent = tempText;
+    
+    let el2 = document.querySelectorAll('.my-little-pony img');
+
+    let src = el2[0].getAttribute('src');
+    let src2 = el2[1].getAttribute('src');
+    el2[0].setAttribute('src', src2);
+    el2[1].setAttribute('src', src);
   }
 }
 

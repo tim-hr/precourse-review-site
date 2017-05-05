62b9c165fb26a28d554f1a51c4636b102f597a5a
Wesley Choi <weschoi.creative@gmail.com>
2017-05-04 17:14:03 -0700

    fear the PandaAssassin

--- public/snippets/prototype-syntax2.js
+++ public/snippets/prototype-syntax2.js
@@ -5,8 +5,8 @@ const Dog = function(name) {
 Dog.prototype.listen = function(humanSpeech) {
   if (humanSpeech.indexOf(this.name) !== -1) {
     return this.bark();
-  }
-}
+  };
+};
 
 Dog.prototype.bark = function() {
   return 'ruff!';

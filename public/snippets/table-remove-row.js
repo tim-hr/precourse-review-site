472778f4dd9bf849162c22460425e79d7e78d546
interdigitize <interdigitize@users.noreply.github.com>
2017-05-05 06:02:47 -0700

    adds row remover and pony swapper capabilities

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,7 @@
-// your code here
+document.getElementById("btn-nuke-first-row").addEventListener("click", removeRow);
+
+function removeRow(){
+  const parent = document.querySelector('tbody');
+  const child = document.querySelector('tbody tr');
+  parent.removeChild(child);
+}

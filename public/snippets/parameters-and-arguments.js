d7f92da8f1a1918a942ddd7bbe34b4cee215758e
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-05 19:23:57 -0700

    added green to header row

--- public/snippets/parameters-and-arguments.js
+++ public/snippets/parameters-and-arguments.js
@@ -1,5 +1,8 @@
+var headerRow = document.querySelector('THEAD TR');
+
 const greenify = function(el) {
-  el.setAttribute('style', 'background-color: green');
+  headerRow.setAttribute('style', 'background-color: green');
+
 };
 
-greenify();
+greenify(headerRow);

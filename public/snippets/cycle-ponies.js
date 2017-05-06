999f37b06502d2574894e9f1628628f9a7a37b0c
Carlos Osorio <carlososoriov@gmail.com>
2017-05-05 22:39:02 -0700

    Implemented nuke button row remover and pony swapping methods

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,8 @@
 // your code here
+const handleSwap = function(){
+  const ponyEl = document.getElementsByClassName('my-little-pony');
+  ponyEl[1].parentNode.insertBefore(ponyEl[1], ponyEl[0]);
+}
+
+const swapButton = document.getElementById('btn-cycle-ponies');
+swapButton.addEventListener('click', handleSwap);
--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,9 @@
 // your code here
+const handleNukeButtonPress = function () {
+  const tableBodyEl = document.querySelector('TABLE TBODY');
+  tableBodyEl.removeChild(tableBodyEl.firstChild);
+  tableBodyEl.removeChild(tableBodyEl.firstChild);
+}
+
+const nukeButton = document.getElementById('btn-nuke-first-row');
+nukeButton.addEventListener('click', handleNukeButtonPress);

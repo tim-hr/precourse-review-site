35822ef8a89702a78ed793fa326e2469435e7462
@jamgorilla <jamesmurphy507@hotmail.com>
2017-05-06 18:18:16 +0100

    ponies working

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,19 @@
-// your code here
+
+let swapBtn = document.getElementById('btn-cycle-ponies');
+
+let num = 0;
+
+swapBtn.addEventListener('click', function(){
+
+    let store1 = document.getElementsByClassName('my-little-pony')[0].getElementsByTagName('img');
+    let store2 = document.getElementsByClassName('my-little-pony')[1].getElementsByTagName('img');
+
+    if (num%2 === 0){
+    store1[0].src="http://i.imgur.com/obV6ULBm.png";
+    store2[0].src="http://i.imgur.com/6PwBk7gm.png";
+    } else {
+    store1[0].src="http://i.imgur.com/6PwBk7gm.png";
+    store2[0].src="http://i.imgur.com/obV6ULBm.png";
+  }
+  num++;
+});

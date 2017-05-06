59036edd82b0b5a957c4a6aeab8b229389b5e9bb
Corey <cry@Coreys-Air.attlocal.net>
2017-05-05 21:39:33 -0700

    Completed 'The command line' section

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,12 @@
 # TODO: list your commands here, one per line.
-
+mkdir client/js client/js/test
+mkdir public public/css public/js
+mv server/server.js index.js
+rm -r server
+rm client/bundle.js
+rm client/multiplier.js
+mv bundle.js public/js
+mv client/app.js client/js
+mv client/index.html public/
+mv client/styles.css public/css
+mv client/test_multiplier.js client/js/test/multiplier.test.js

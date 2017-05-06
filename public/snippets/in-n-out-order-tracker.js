5026a6ab70bc2ab5e68ecc3d03ddc75853d76c68
@jamgorilla <jamesmurphy507@hotmail.com>
2017-05-06 16:59:49 +0100

    Classes states and behaviour

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,6 +1,34 @@
 class Order {
+  
+ constructor(){
+   this.array = {
+   	  states: ['Taking order', 'Cooking order', 'Requesting pickup of order', 'Order complete']
+   };
+   this.num = 0;
+ };
 
-}
+ returnState(){
+   return this.array['states'][this.num];
+ };
+
+ advanceState(){
+ 	this.num++;
+ };
+
+};
+
+const order1 = new Order();
+const order2 = new Order();
+
+order1.advanceState();
+order1.advanceState();
+order2.advanceState();
+order1.advanceState();
+
+console.log(order1.returnState());
+console.log(order2.returnState());
+
+
 
 // create order instance #1
 // create order instance #2
@@ -10,3 +38,4 @@ class Order {
 // advance state of #1
 // print state of #1 to console
 // print state of #2 to console
+ 

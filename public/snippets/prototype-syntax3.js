3382ffa22c22816a726f56f3c8ed026ee2dce6f1
@jamgorilla <jamesmurphy507@hotmail.com>
2017-05-06 16:06:04 +0100

    Classes as an alternative syntax for prototypes complete

--- public/snippets/prototype-syntax3.js
+++ public/snippets/prototype-syntax3.js
@@ -1,1 +1,10 @@
-// const PandaAssassin = ?
+ const PandaAssassin = function(){
+ };
+
+ PandaAssassin.prototype.preferredWeapon = function(preferredWeapon){
+    this.preferredWeapon = preferredWeapon;
+ }
+
+ PandaAssassin.prototype.terminate = function(target) {
+ 	return `${target} terminated with ${this.preferredWeapon}.`;
+ };

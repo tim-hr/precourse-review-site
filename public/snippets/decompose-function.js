8cc82f56bc670e303561d84ada5b02e9893a0b73
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-05 19:34:06 -0700

    added pseudocode to helper functions

--- public/snippets/decompose-function.js
+++ public/snippets/decompose-function.js
@@ -1,11 +1,17 @@
 const renderColumnSums = function() {
-  // your pseudocode here
+  // find the footer element for column index
+  // set its text content to the sum found in addCellValues
 };
 
-const TODO = function() {
-  // your pseudocode here
+const findCellValues = function() {
+  // fetch all the cell values for that column from the model
+  //  use a for-loop to iterate through the column
+  //  use !isNaN to filter out the non-numeric values from the column list
 };
 
-const TODO = function() {
-  // your pseudocode here
+const addCellValues = function() {
+  // add up the filtered values in thee list
+  //  write an add formula
+  //  use findCellValues to catch the numeric values
+  //  implement findCellValues to add formula
 };
--- public/snippets/parameters-and-arguments.js
+++ public/snippets/parameters-and-arguments.js
@@ -1,8 +1,7 @@
-var headerRow = document.querySelector('THEAD TR');
+let headerRow = document.querySelector('THEAD TR');
 
 const greenify = function(el) {
   headerRow.setAttribute('style', 'background-color: green');
-
 };
 
 greenify(headerRow);

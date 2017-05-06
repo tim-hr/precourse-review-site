56456132af36936a596271590258e5cae1bb094f
@jamgorilla <jamesmurphy507@hotmail.com>
2017-05-06 17:38:37 +0100

    table remove row complete

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,12 @@
 // your code here
+
+let EliminateBtn = document.getElementById('btn-nuke-first-row');
+let clickCount = 1;
+
+EliminateBtn.addEventListener('click', function(){
+ let row = document.getElementsByTagName('td');
+ for (var i = 0; i < 3*clickCount; i++){
+   row[i].innerHTML = '';
+ };
+clickCount++;
+});

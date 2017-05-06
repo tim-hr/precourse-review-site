ce4820cdaeaa4b9c119b9759b45ce316b5e15438
David Vassett <david@davidvassett.com.au>
2017-05-05 18:38:15 -0700

    Completed: Selectors

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,14 @@
-// your code here
+const cyclePonies = function () {
+  const cyclePoniesButton = document.querySelector('#btn-cycle-ponies');
+
+  cyclePoniesButton.addEventListener('click', function () {
+    const divs = document.querySelectorAll('.my-little-pony');
+    const image1 = divs[0].querySelector('IMG').src;
+    const image2 = divs[1].querySelector('IMG').src;
+    
+    divs[0].querySelector('IMG').src = image2;
+    divs[1].querySelector('IMG').src = image1;
+  });
+};
+
+cyclePonies();
--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,13 @@
-// your code here
+const removeFirstRow = function () {
+  const removeRowButton = document.querySelector('#btn-nuke-first-row');
+
+  removeRowButton.addEventListener('click', function () {
+    const parent = document.querySelector('.data-table TBODY');
+
+    if (parent.children.length > 0) {
+      parent.removeChild(parent.firstElementChild);
+    }
+  });
+};
+
+removeFirstRow();

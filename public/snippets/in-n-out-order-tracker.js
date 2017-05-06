0ff9129f7d5a9af60fb6fd82e1f189b40c6f2e3b
dandesonjew <danieltanderson@berkeley.edu>
2017-05-06 10:46:31 -0700

    coded in-and-out orders

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,12 +1,34 @@
 class Order {
+  constructor() {
+    this.state = 0;
+  }
+  advanceOrder() {
+    if(this.state < 4) {
+      this.state = this.state + 1;
+    } else {
+      console.log("This order has already been completed");
+    }
+  }
+
+  currentState() {
+    if(this.state == 0) {
+      console.log("Taking order");
+    }else if(this.state == 1) {
+      console.log("Cooking order");
+    }else if(this.state == 2) {
+      console.log("Requesting Pickup Of Order");
+    }else if(this.state == 3) {
+      console.log("Order Complete");
+    }
+  }
 
 }
 
-// create order instance #1
-// create order instance #2
-// advance state of #1
-// advance state of #1
-// advance state of #2
-// advance state of #1
-// print state of #1 to console
-// print state of #2 to console
+const order1 = new Order();
+const order2 = new Order();
+order1.advanceOrder();
+order1.advanceOrder();
+order2.advanceOrder();
+order1.advanceOrder();
+order1.currentState();
+order2.currentState();

139b09f7bf3bb8a3ba75af4587072a9a3b28ca71
@jamgorilla <jamesmurphy507@hotmail.com>
2017-05-06 17:18:20 +0100

    Navigating and manipulating the directory complete

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,24 @@
 # TODO: list your commands here, one per line.
 
+# rm bundle.js
+ 
+# mkdir public
+# mkdir js
+# mkdir css
+
+# mv /client/styles.css /public/css/styles.css
+# mv /client/index.html /public/index.html
+# mv /bundle.js /public/js/bundle.js
+
+# mv /server/server.js ./index.js
+# rm /server
+
+# mkdir client/js
+
+# mv /client/app.js /client/js/app.js
+
+# mkdir client/test
+
+# mv /test_multiplier.js /test_multiplier.test.js
+
+

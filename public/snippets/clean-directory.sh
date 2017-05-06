d7dafd167a4001158bba8cff8c3ad32c0b036952
dandesonjew <danieltanderson@berkeley.edu>
2017-05-06 10:47:46 -0700

    wrote clean directory to manipulate the directory structure

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,23 @@
-# TODO: list your commands here, one per line.
+mkdir -p public/css
 
+mkdir public/js
+
+mkdir -p client/js/test
+
+mv client/app.js client/js/app.js
+
+rm client/bundle.js
+
+mv bundle.js public/js/bundle.js
+
+mv client/index.html public/index.html
+
+rm client/multiplier.js
+
+mv client/style.css public/css/style.css
+
+mv client/test_multiplier.js client/js/test/multiplier.test.js
+
+mv server/server.js index.js
+
+rm -rf server

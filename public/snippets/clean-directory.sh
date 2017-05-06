df7e0e635f0ce1f3537b2dcf0a03fc56f4a89cea
David Vassett <david@davidvassett.com.au>
2017-05-05 17:36:00 -0700

    Completed: Navigating and manipulating a directory hierarchy

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,14 @@
-# TODO: list your commands here, one per line.
-
+mv server/server.js index.js;
+mv server public;
+mkdir public/css;
+mkdir public/js;
+rm bundle.js
+cd client;
+mkdir js;
+mkdir js/test;
+mv app.js js
+mv test_multiplier.js js/test/multiplier.test.js;
+mv index.html ../public;
+mv bundle.js ../public/js;
+mv styles.css ../public/css;
+rm multiplier.js;

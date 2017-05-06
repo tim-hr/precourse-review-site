83f338e02b25383f8847bb06b31cb69896a19fb9
dandesonjew <danieltanderson@berkeley.edu>
2017-05-06 11:12:44 -0700

    completed row1

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,29 @@
-// your code here
+
+class TableRemoveRow {
+
+  constructor() {
+    this.sheetBodyEl = document.querySelector('TBODY');
+    this.nukeButton = document.querySelector('#btn-nuke-first-row');
+  }
+
+  attachEventHandlers() {
+    this.nukeButton.addEventListener('click', 
+    this.nukeRowClick.bind(this));
+  }
+
+  nukeRowClick(evt){
+    //console.log("the nuke button was clicked");
+    this.removeChildren(this.sheetBodyEl.rows[0]);
+
+  }
+  
+  removeChildren(parentEl) {
+    while(parentEl.firstChild) {
+      parentEl.removeChild(parentEl.firstChild);
+    }
+  }
+}
+
+const table = new TableRemoveRow();
+table.attachEventHandlers();
+

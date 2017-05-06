4c362cfe13f0e5a23574c35f0a8ba509c2f3ddff
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-05 18:46:10 -0700

    Revert "add pseudocode for helper functions"
    This reverts commit 6ed68b500ce0ecbc4c7d5d84597afb709dac5255.

--- public/snippets/decompose-function.js
+++ public/snippets/decompose-function.js
@@ -1,16 +1,11 @@
 const renderColumnSums = function() {
-  // find the footer element for a particular column
-  //  set its text content to the sum
-  //  use addCellValues function here to render sum
+  // your pseudocode here
 };
 
-const findCellValues = function() {
-  // iterate through the column with a for-loop
-  // filter out the non-numeric values
-  //  use !isNaN to filter out the non-numeric values
+const TODO = function() {
+  // your pseudocode here
+};
 
-const addCellValues = function() {
-  // implement findCellValues function
-  //  iterate through cell values with a for-loop
-  //  add the cell values
+const TODO = function() {
+  // your pseudocode here
 };

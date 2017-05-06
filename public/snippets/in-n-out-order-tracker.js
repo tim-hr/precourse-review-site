1be1e7fa6c0ba91c023232e19a0e24a45e89aadd
kluwena <karelluwena@gmail.com>
2017-05-06 09:31:17 -0700

    fixed directory hierarchy

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,12 +1,38 @@
 class Order {
 
-}
+  contructor(){
+    this.data = {
+      state: ["Taking order", "Cooking order", "Request picking up of order", "Order complete"]
+    },
+    this.place = 0
+  };
+
+  trackState(){
+    return this.data['state'][this.place]
+  };
+
+  advanceState(){
+    this.place++;
+  };
+};
 
 // create order instance #1
 // create order instance #2
+const order1 = new Order();
+const order2 = new Order();
+
 // advance state of #1
 // advance state of #1
 // advance state of #2
 // advance state of #1
+order1.advanceState();
+order1.advanceState();
+order2.advanceState();
+order2.advanceState();
+
 // print state of #1 to console
 // print state of #2 to console
+console.log(order1.trackState());
+console.log(order2.trackState());
+
+

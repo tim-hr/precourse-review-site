56f6490f6e63bf74a51f3f1a1eceb984b3709f30
@jamgorilla <jamesmurphy507@hotmail.com>
2017-05-06 15:37:56 +0100

    Function basics complete

--- public/snippets/decompose-function.js
+++ public/snippets/decompose-function.js
@@ -1,11 +1,14 @@
 const renderColumnSums = function() {
-  // your pseudocode here
+  //   find the footer element for this column
+  //   set its text content to the return from sumColumnValues()
 };
 
-const TODO = function() {
-  // your pseudocode here
+const sumColumnValues = function() {
+  //   add up the filtered values returned from fetchColumnValues()
 };
 
-const TODO = function() {
-  // your pseudocode here
-};
+const fetchColumnValues = function() {
+  //   for each col index up to the num of cols
+  //   fetch all the cell values for that column from the model
+  //   filter out the non-numeric values from this list
+}; 
--- public/snippets/parameters-and-arguments.js
+++ public/snippets/parameters-and-arguments.js
@@ -1,5 +1,15 @@
-const greenify = function(el) {
-  el.setAttribute('style', 'background-color: green');
+
+ var x = document.getElementsByTagName('th')[2];
+ var y = document.getElementsByTagName('th')[3];
+
+const greenify = function(el1, el2) {
+
+  el1.style.backgroundColor = 'green';
+  el2.style.backgroundColor = 'green';
 };
 
-greenify();
+
+
+greenify(x, y);
+
+

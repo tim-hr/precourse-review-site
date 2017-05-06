dce50b27f7361956dd13415af001de984165d1d5
Jun Park <jun.park@live.com>
2017-05-06 14:38:47 -0600

    tried to finish Clock functionality but couldn't get it to work. The rest of Review1 is now finished.

--- public/snippets/clock.js
+++ public/snippets/clock.js
@@ -1,7 +1,13 @@
-const clock = {
-  getHours: new Date().getHours(),
-  getMinutes: new Date().getMinutes(),
-  getSeconds: new Date().getSeconds()
-};
+// I can't seem to get this one figured out no matter how many hints I read
 
+
+let hour = new Date().getHours();
+let minute = new Date().getMinutes();
+let second = new Date().getSeconds();
+
+const clock = {
+  getHours: hour,
+  getMinutes: minute,
+  getSeconds: second
+};
 

63f0a019e5515a68c37c8fba556104b496e2ad4f
dandesonjew <danieltanderson@berkeley.edu>
2017-05-06 11:12:44 -0700

    completed row1

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,24 @@
-// your code here
+
+class CyclePonies {
+
+  constructor() {
+    this.swapButton = document.querySelector('#btn-cycle-ponies');
+    this.firstPony = document.getElementsByClassName('my-little-pony');
+    console.log(this.firstPony);
+  }
+
+  attachEventHandlers() {
+
+    this.swapButton.addEventListener('click', this.swapPoniesClick.bind(this));
+  }
+
+  swapPoniesClick(evt) {
+    const pony = this.firstPony[1];
+    pony.parentNode.insertBefore(pony,pony.parentNode.children[9]);
+  }
+
+}
+
+const ponySwap = new CyclePonies();
+ponySwap.attachEventHandlers();
+

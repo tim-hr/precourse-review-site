d7b0595048ef4fe4a96b74dcd86ac73e69fce456
kluwena <karelluwena@gmail.com>
2017-05-06 13:11:40 -0700

    fixed selector

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -16,22 +16,14 @@ class Order {
   };
 };
 
-// create order instance #1
-// create order instance #2
 const order1 = new Order();
 const order2 = new Order();
 
-// advance state of #1
-// advance state of #1
-// advance state of #2
-// advance state of #1
 order1.advanceState();
 order1.advanceState();
 order2.advanceState();
 order2.advanceState();
 
-// print state of #1 to console
-// print state of #2 to console
 console.log(order1.trackState());
 console.log(order2.trackState());
 

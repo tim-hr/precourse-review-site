7c24ad549015408ed262f3b717c7330afd9169c8
gbaik <gideonbaik@gmail.com>
2017-05-06 06:39:31 -0700

    Add factory function

--- public/snippets/greeting-factory-function.js
+++ public/snippets/greeting-factory-function.js
@@ -1,5 +1,7 @@
 const makeGreetingFunction = function(greeting) {
-  // TODO
+  return function(name){
+    return '"' + greeting + ', ' + name + '!"';
+  };
 };
 
 const greet1 = makeGreetingFunction('Salutations');

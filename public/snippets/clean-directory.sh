15f30141d5aa3a3a4770366fcbbdc842dc3eb8bc
Emanuel <emanuel@Emanuels-MacBook-Pro.local>
2017-05-05 21:05:21 -0700

    added unix command lines

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,23 @@
 # TODO: list your commands here, one per line.
+# rm bundle.js
+# cd client
+# mkdir js
+# mv app.js client/js
+# mkdir test
+# mv test_multiplier.js multiplier.test.js
+# mv multiplier.test.js client/js/test
+# cd ..
+# cd ..
+# rm multiplier.js
+# cd ..
+# touch index.js
+# rmdir server
+# mkdir public
+# cd public
+# mkdir css
+# mv styles.css public/css
+# cd ..
+# mv index.html public
+# mkdir js
+# mv bundle.js public/js
 

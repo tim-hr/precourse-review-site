b2523a52633c7eb26e2c18a364f1591d3011ce2e
Kirk Rohani <kirk.rohani@gmail.com>
2017-05-05 17:40:04 -0700

    completed selectors

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,27 @@
-// your code here
+
+const cyclePoniesButton = document.querySelector("#btn-cycle-ponies");
+
+const swapPonies = function() {
+    const imageDivs = document.querySelectorAll(".my-little-pony");
+    let firstImageTag = imageDivs[0].getElementsByTagName('img')[0];
+    let firstImageNameDiv = imageDivs[0].firstElementChild;
+
+    let secondImageTag = imageDivs[1].getElementsByTagName('img')[0];
+    let secondImageNameDiv = imageDivs[1].firstElementChild;
+
+    let tempImageTag = firstImageTag.src;    
+    let tempImageName = firstImageNameDiv.textContent;
+
+    firstImageTag.src = secondImageTag.src;
+    secondImageTag.src = tempImageTag;
+   
+    firstImageNameDiv.textContent = secondImageNameDiv.textContent;
+    secondImageNameDiv.textContent = tempImageName;
+
+};
+
+cyclePoniesButton.addEventListener("click", swapPonies);
+
+
+
+
--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,12 @@
-// your code here
+const nukeFirstRowButton = document.querySelector("#btn-nuke-first-row");
+
+const nukeRow = function() {
+    const dataTableBodyEl = document.querySelector(".data-table tbody");
+    if (dataTableBodyEl.childNodes[0].nextElementSibling) {
+        dataTableBodyEl.deleteRow(0);
+    }
+};
+
+nukeFirstRowButton.addEventListener("click", nukeRow);
+
+

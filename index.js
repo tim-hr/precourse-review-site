9680affe0d5ebf3336a0bbb7d3f2cdf74c6921fb
Jacob Sklar <jsklar.js@gmail.com>
2017-05-07 15:24:11 -0700

    switched port from 3000 to 9000

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 9000);
 
 const server = http.createServer(app);
 server.listen(app.get('port'), () => {

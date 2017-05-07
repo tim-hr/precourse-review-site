48ca03e3491aaebf98a4806f177e97e925a0e58a
Corey <cry@Coreys-Air.attlocal.net>
2017-05-07 15:35:34 -0700

    Completed Selectors page

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,39 @@
+class TableManager {
+
+  connectEventHandlers() {
+      document.getElementById('btn-nuke-first-row')
+        .addEventListener(
+          'click',
+          this.onSubmit.bind(this));
+      }
+
+  onSubmit(event) {
+      event.preventDefault();
+
+      const currentHead = document.querySelector('TBODY TR');
+      currentHead.remove()
+      }
+
+  };
+
+  const tableManager = new TableManager();
+  tableManager.connectEventHandlers();
+
+/*
+  const nukeButton = document.getElementById('btn-nuke-first-row');
+  const topRow = document.querySelector('TBODY TR');
+  console.log(topRow);
+
+
+  const removeTopRow = function() {
+    event.preventDefault();
+
+    return console.log('tried');
+
+  }
+
+  nukeButton.addEventListener('click', removeTopRow());
+};
 // your code here
+   
+*/

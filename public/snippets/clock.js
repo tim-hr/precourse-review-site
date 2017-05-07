c38c6ccaea5cfd3c4039303cfe9f722950513131
azmeer <azmeer.salleh@gmail.com>
2017-05-07 14:32:00 -0700

    Adding solution for methods/clock.js

--- public/snippets/clock.js
+++ public/snippets/clock.js
@@ -2,7 +2,7 @@
 const clock = {
   getHours: function() { return (new Date()).getHours(); },
   getMinutes: function() { return (new Date()).getMinutes(); },
-  getSeconds: function() { return (new Date()).getSeconds(); },
+  getSeconds: function() { return (new Date()).getSeconds(); }
 };
 
 

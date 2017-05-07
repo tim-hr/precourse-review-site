75f65c8d867d9c32ecffa57cbe55d010905afcfc
azmeer <azmeer.salleh@gmail.com>
2017-05-07 09:44:15 -0700

    Adding answer for deleting table rows

--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,23 @@
 // your code here
+
+class ViewManager {
+
+  connectEventHandlers() {
+    document.getElementById('btn-nuke-first-row')
+      .addEventListener(
+        'click',
+        this.onClickNukeFirstRow);
+  }
+
+  onClickNukeFirstRow(event) {
+    const tableBodyEl = document.querySelector('TBODY');
+    if (tableBodyEl.firstElementChild) {
+      tableBodyEl.removeChild(tableBodyEl.firstElementChild);
+    }
+  }
+
+}
+
+const viewManager = new ViewManager();
+viewManager.connectEventHandlers();
+

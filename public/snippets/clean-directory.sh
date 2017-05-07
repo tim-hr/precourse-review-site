21d0714ba367842bb2a529de7a22568078e4e3c4
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-05-07 09:17:55 -0700

    Finished command line

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,15 @@
 # TODO: list your commands here, one per line.
+rm server *
+touch index.js
+mkdir public
+mkdir public/css
+mkdir public/js
+mv client/styles.css public/css
+mv client/index.html public
+mv client/bundle.js public/js
+mkdir client/js
+mkdir client/js/test
+mv client/app.js client/js
+mv client/test.multiplier.js client/js/test
+
 

da963b8d91f0790cf562bf4d859a2a12454f6986
gbaik <gideonbaik@gmail.com>
2017-05-07 03:42:16 -0700

    Add selectinator 5000 functions

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,12 @@
-// your code here
+function swapImagePositions() {
+  const firstImage = document.querySelectorAll(".my-little-pony")[0];
+  const secondImage = document.querySelectorAll(".my-little-pony")[1];
+
+  const image = document.querySelectorAll('.my-little-pony');
+
+  firstImage.insertBefore(secondImage, firstImage.childNodes[0]);
+
+}
+document.getElementById('btn-cycle-ponies').addEventListener('click', swapImagePositions);
+
+//This was as far as I was able to make it, I can't seem to find a way to reset it back to the original state
--- public/snippets/table-remove-row.js
+++ public/snippets/table-remove-row.js
@@ -1,1 +1,7 @@
-// your code here
+function deleteFirstRow() {
+  const tableBody = document.getElementsByTagName('tbody')[0];
+  const firstTableRow = tableBody.getElementsByTagName('tr')[0];
+  firstTableRow.parentNode.removeChild(firstTableRow);
+}
+
+document.getElementById('btn-nuke-first-row').addEventListener('click', deleteFirstRow);

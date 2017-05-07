10e83cfb4c8a71bcb7cdaf2171470171cfc0438b
Benjamin Bucca <benjamin.bucca3@gmail.com>
2017-05-07 09:00:04 -0700

    Finished classes for review site

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,12 +1,37 @@
 class Order {
 
+  constructor(order) {
+    this.state = 'Order up';
+  }
+
+  isCooking() {
+    this.state = 'Order cooking';
+  }
+
+  isRequestingPickup() {
+    this.state = 'Order requested for pickup';
+  }
+
+  isComplete() {
+    this.state = 'Order ready!';
+  }
+
 }
 
+
 // create order instance #1
+var order1 = new Order();
 // create order instance #2
+var order2 = new Order();
 // advance state of #1
+order1.isCooking();
 // advance state of #1
+order1.isRequestingPickup();
 // advance state of #2
+order2.isCooking();
 // advance state of #1
+order1.isComplete();
 // print state of #1 to console
+console.log(order1.state);
 // print state of #2 to console
+console.log(order2.state);

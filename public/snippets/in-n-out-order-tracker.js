6402573d82c2720ec5d6f714bd7e767276b81835
gbaik <gideonbaik@gmail.com>
2017-05-07 04:01:00 -0700

    Add in-n-out tracker function

--- public/snippets/in-n-out-order-tracker.js
+++ public/snippets/in-n-out-order-tracker.js
@@ -1,12 +1,24 @@
 class Order {
 
+  constructor(order) {
+    // create order instance #1
+  // create order instance #2
+  }
+
+  cook() {
+    // advance state of #1
+    // advance state of #2
+  }
+
+  pickUp() {
+    // advance state of #1
+  // advance state of #2
+  }
+
+  giveOrder() {
+    // print state of #1 to console
+  // print state of #2 to console
+  }
+
 }
 
-// create order instance #1
-// create order instance #2
-// advance state of #1
-// advance state of #1
-// advance state of #2
-// advance state of #1
-// print state of #1 to console
-// print state of #2 to console

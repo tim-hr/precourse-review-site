cca8c5934733d36df79a2dd6bf1a3557d5aa9498
Yermek Alzanov <yermek-alzhanov@utulsa.edu>
2017-05-08 06:10:39 -0500

    DOM Selectors review finished

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,31 @@
-// your code here
+class ViewManagerPony {
+
+  connectEventHandlers() {
+    document.getElementById('btn-cycle-ponies')
+      .addEventListener(
+        'click',
+        this.onClick.bind(this))
+  }
+
+  onClick() {
+    ////TEXT SWAP
+    const ponyText = document.querySelectorAll('.my-little-pony div');
+    const text1 = ponyText[0].textContent
+    const text2 = ponyText[1].textContent
+    const textBuffer = text1;
+    ponyText[0].textContent = text2;
+    ponyText[1].textContent = textBuffer;
+
+    ////IMAGE SWAP
+    const ponyImage = document.querySelectorAll('.my-little-pony img');
+    const img1 = ponyImage[0].src;
+    const img2 = ponyImage[1].src;
+    const imgBuffer = img1;
+    ponyImage[0].src = img2;
+    ponyImage[1].src = imgBuffer;
+
+  }
+}
+
+const viewManagerPony = new ViewManagerPony();
+viewManagerPony.connectEventHandlers();

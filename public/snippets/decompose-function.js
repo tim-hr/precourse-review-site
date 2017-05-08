0f4fe2b4dcc6ed32565e32388d6022d46e392038
Yuki Omori <yuki.omori@inte.co.jp>
2017-05-08 12:57:57 +0900

    completed Good functions do just one thing

--- public/snippets/decompose-function.js
+++ public/snippets/decompose-function.js
@@ -1,11 +1,17 @@
 const renderColumnSums = function() {
-  // your pseudocode here
+  // for each column  
+    // calculate colmun sum
+    // render the sum to the textContent of the footer
 };
 
-const TODO = function() {
-  // your pseudocode here
+const calculateColumnSum = function() {
+  // fetch all the cell values for that column from the model
+  // filter out the non-numeric values from this list
+  // add up the filtered values in the list
 };
 
-const TODO = function() {
-  // your pseudocode here
+const renderSumToFooter = function() {
+  // fetch a sum of a column
+  // find the footer element for this column
+  // set its text content to the sum
 };

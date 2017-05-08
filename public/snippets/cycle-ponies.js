60ee533e6627ab7fbcc6dd25aee2fb71341b251f
Jesus Esquivel <JAER@Jesuss-MacBook-Air.local>
2017-05-07 20:07:48 -0700

    Dom Manipulation

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,9 @@
-// your code here
+const cyclePoniesEl = document.querySelectorAll('.my-little-pony');
+cyclePoniesEl.addEventListener('click', function() {
+    for(let i = 0; i < cyclePoniesEl.length; i++){
+      let current = cyclePoniesEl[i];
+      let next = cyclePoniesEl[i + 1];
+      
+      current = next;
+    }
+});

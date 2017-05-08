2aec79707628612ab2d1c7a0744fbd0f7db9000e
Jacob Sklar <jsklar.js@gmail.com>
2017-05-07 21:36:25 -0700

    added commands to modify tree

--- public/snippets/clean-directory.sh
+++ public/snippets/clean-directory.sh
@@ -1,2 +1,15 @@
-# TODO: list your commands here, one per line.
-
+mkdir client/js
+mkdir client/js/test
+mkdir public
+mkdir public/css
+mkdir public/js
+mv server/server.js .
+rmdir server
+rm client/multiplier.js
+mv client/app.js client/js
+mv client/test_multiplier.js client/js/test
+mv client/js/test/test_multiplier.js client/js/test/multiplier.test.js
+mv server.js index.js
+touch public/css/styles.css
+mv client/index.html public/index.html
+mv client/bundle.js public/js

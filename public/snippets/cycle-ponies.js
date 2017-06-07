0b38b838c8d2c7354ef9b3aac66926436bd973af
Yuki Omori <yuki.omori@inte.co.jp>
2017-06-07 16:55:23 +0900

    completed Selectinator 5000 except for bind error(but it works)

--- public/snippets/cycle-ponies.js
+++ public/snippets/cycle-ponies.js
@@ -1,1 +1,29 @@
 // your code here
+
+class SwapImages {
+
+  connectEventHandlers() {
+
+    document.getElementById('btn-cycle-ponies')
+      .addEventListener(
+        'click',
+        this.onSubmit.bind(this));
+  }
+
+  swapElements (element1, element2) {
+    let clonedElement1 = element1.cloneNode(true);
+    let clonedElement2 = element2.cloneNode(true);
+    element1.parentNode.replaceChild(clonedElement2, element1);
+    element2.parentNode.replaceChild(clonedElement1, element2);
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    let ponyImages = document.getElementsByClassName('my-little-pony');
+    this.swapElements(ponyImages[0], ponyImages[ponyImages.length - 1]).bind(this);
+  }
+}
+
+const swapImages = new SwapImages();
+swapImages.connectEventHandlers();
